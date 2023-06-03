ecc0842ee2d4120037f3459cb2656a82fcab841d
Tatu Anttila <tatu.anttila@tuni.fi>
2023-06-03 17:31:03 +0300

    harjoitus 1.10

--- unicafe/src/App.js
+++ unicafe/src/App.js
@@ -8,12 +8,12 @@ import { useState } from 'react'
       return ( 
         <div>
           <h1>statistics</h1>
-          <p>good {props.good}</p>
-          <p>neutral {props.neutral}</p>
-          <p>bad {props.bad}</p>
-          <p>all {props.total}</p>
-          <p>average {(props.good - props.bad) / props.total}</p>
-          <p>positive {(props.good / props.total)*100} %</p>    
+          <StatisticLine text="good" value = {props.good} />
+          <StatisticLine text="neutral" value = {props.neutral} />
+          <StatisticLine text="bad" value = {props.bad} />
+          <StatisticLine text="all" value = {props.total} />
+          <StatisticLine text="average" value = {(props.good - props.bad) / props.total} />
+          <StatisticLine text="positive" value = {(props.good / props.total)*100 + ' %'}  />
         </div>
       )
     }
@@ -28,19 +28,27 @@ import { useState } from 'react'
     }
   }
 
+
+  const StatisticLine = props => {
+    return(
+      <p>{props.text} {props.value}</p>
+    )
+  }
+
+  // Button - komponentti
+  const Button = (props) => {
+    return(
+    <button onClick={props.handleClick}>
+      {props.text}
+    </button>)
+  }
+
 function App() {
   // tallenna napit omaan tilaansa
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
   const [total, setTotal] = useState(0)
-
-  // Button - komponentti
-  const Button = ({ handleClick, text }) => (
-    <button onClick={handleClick}>
-      {text}
-    </button>
-  )
 
   // nappien painamiseen liittyvät eventit
   const increaseGood = () => {
