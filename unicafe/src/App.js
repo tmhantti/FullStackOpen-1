ca6d912897d8a4ed23d1be5e5b1cdd917b60f419
Tatu Anttila <tatu.anttila@tuni.fi>
2023-06-03 17:29:29 +0300

    harjoitus 1.7

--- unicafe/src/App.js
+++ unicafe/src/App.js
@@ -5,6 +5,7 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const [total, setTotal] = useState(0)
 
   // Button - komponentti
   const Button = ({ handleClick, text }) => (
@@ -14,9 +15,24 @@ const App = () => {
   )
 
   // nappien painamiseen liittyvät eventit
-  const increaseGood = () => setGood(good + 1)
-  const increaseNeutral = () => setNeutral(neutral + 1)
-  const increaseBad = () => setBad(bad + 1)
+  const increaseGood = () => {
+    const newGood = good + 1
+    setGood(newGood)
+    setTotal(newGood + neutral + bad)
+  }
+  const increaseNeutral = () => {
+    const newNeutral = neutral + 1
+    setNeutral(newNeutral)
+    setTotal(good + newNeutral + bad)
+  }
+  const increaseBad = () => {
+    const newBad= bad + 1
+    setBad(newBad)
+    setTotal(good + neutral + newBad)
+  }
+
+  // statistiikkaan liittyvät funktiot:
+
 
   return (
     <div>
@@ -37,6 +53,9 @@ const App = () => {
       <p>good {good}</p>
       <p>neutral {neutral}</p>
       <p>bad {bad}</p>
+      <p>all {total}</p>
+      <p>average {(good - bad)/total}</p>
+      <p>positive {(good/total)*100} %</p>
     </div>
   )
 }
